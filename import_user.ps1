e8e75eafa97cc643a4c52ce103df0c27b3b37508
Joe <cheasingh168@gmail.com>
2018-08-02 10:45:54 +0700

    Update import_user.ps1

--- import_user.ps1
+++ import_user.ps1
@@ -1,3 +1,2 @@
-import-csv c:\users\raksmey\desktop\third_sheet.csv | foreach-object { $userprincipalname = $_.SamAccountName + "@lab.edu" 
-New-ADUser -SamAccountName $_.SamAccountName -UserPrincipalName $userprincipalname -Name $_.name -DisplayName $_.name -GivenName $_.cn -SurName $_.sn -Department $_.Department -Path "OU=Users,OU=BMET,OU=University,DC=lab,DC=edu
-" -Officephone $_.OfficePhone -ScriptPath $_.Scriptpath -Company $_.Company -AccountPassword (ConvertTo-SecureString -AsPlainText $_.Password -force) -Enabled $True -PasswordNeverExpires $True -PassThru }
+import-csv [filelocation.csv] | foreach-object { $userprincipalname = $_.SamAccountName + ["@test.com"] 
+New-ADUser -SamAccountName $_.SamAccountName -UserPrincipalName $userprincipalname -Name $_.name -DisplayName $_.name -GivenName $_.cn -SurName $_.sn -Department $_.Department -Path ["OU=Users,DC=test,DC=edu"] -Officephone $_.OfficePhone -ScriptPath $_.Scriptpath -Company $_.Company -AccountPassword (ConvertTo-SecureString -AsPlainText $_.Password -force) -Enabled $True -PasswordNeverExpires $True -PassThru }
