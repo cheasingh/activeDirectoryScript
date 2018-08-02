47ae628c1fe344353360d20c32b48862dc21452e
Joe <cheasingh168@gmail.com>
2018-08-02 10:54:33 +0700

    Update Modifications_ADUSER.ps1

--- Modifications_ADUSER.ps1
+++ Modifications_ADUSER.ps1
@@ -1,6 +1,6 @@
 import-module activedirectory
 
-$users = import-csv -path c:\users\administrator\desktop\users.csv
+$users = import-csv -path [filelocation.csv]
 
 foreach ($user in $users) {
 	Get-ADUser -Filter "SamAccountName -eq '$($user.samaccountname)'" -properties * -searchbase "ou=IT,dc=region,dc=com"|set-aduser -replace @{l="$($user.city)"; physicalDeliveryOfficeName = "$($user.Office)";division="$($user.Division)"}}
