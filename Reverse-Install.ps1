aad11547de2d713fa05aa0e640208dbb3e31bf31
Joe <cheasingh168@gmail.com>
2018-08-02 10:50:20 +0700

    Update Reverse-Install.ps1

--- Reverse-Install.ps1
+++ Reverse-Install.ps1
@@ -1,7 +1,5 @@
-#5. Create a DNS reverse lookup zone
-
+#Create a DNS reverse lookup zone
 Add-DnsServerPrimaryZone 5.168.192.in-addr.arpa -ZoneFile 5.168.192.in-addr.arpa.dns
 
-#6. Create PTR
-
+#Create PTR
 Add-DnsServerResourceRecordPtr -ZoneName 5.168.192.in-addr.arpa -Name 1 -PtrDomainName ad1.Region.com
